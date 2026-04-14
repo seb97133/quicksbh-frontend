0ec79fe92d7a2b44744e0f3a1fe6a961677e2837
seb97133 <sebdunoye@orange.fr>
2026-04-13 20:36:17 -0400

    Déploiement du build sur Vercel

---
+++ build/static/js/488.61ac16f4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkquicksbh_frontend=globalThis.webpackChunkquicksbh_frontend||[]).push([[488],{488(e,n,h){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.61ac16f4.chunk.js.map
